482bbc184a2ff3b75535ad0e135789a41a93666a
sunnysuwalka <thesunnny21@gmail.com>
2026-08-20 12:52:55 +0530

    corrected auth.js Caps issue

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser } from '../controllers/AuthController.js';
+import {loginUser} from "../controllers/authController.js"
 
 const router = express.Router();
 router.post('/login', loginUser);
